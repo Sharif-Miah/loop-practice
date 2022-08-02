db18e1795653ecec09300c9a69c6554a72e6bdcd
farhan sharif <farhansharif1410@gmail.com>
2022-08-02 08:40:19 -0230

    third commit

--- reverse.js
+++ reverse.js
@@ -1,10 +1,18 @@
 
-// let number = 10;
+let number = 10;
 
-// while (number >= 1) {
-//     console.log(number);
-//     number--;
-// }
+while (number >= 1) {
+    console.log(number);
+    number--;
+}
+
+
+for (i = 10; i >= 1; i--) {
+    if (i % 2 === 1) {
+        console.log(i);
+    }
+}
+
 
 
 
